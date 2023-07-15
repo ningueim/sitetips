0a8906b2a4ed633af7cc47b240cbf976f483f1b3
ningueim <136115006+ningueim@users.noreply.github.com>
2023-07-15 14:12:07 -0300

    Add files via upload

--- java.js
+++ java.js
@@ -17,3 +17,27 @@ fetch('1.csv')
       tabela.appendChild(novaLinha);
     });
   });
+  fetch('dados.json')
+  .then(response => response.json())
+  .then(data => {
+    const divDados = document.getElementById('dados');
+
+    // Limpar o conteúdo atual da div
+    divDados.innerHTML = '';
+
+    // Manipular os dados para exibição no HTML
+    data.forEach(item => {
+      const p = document.createElement('p');
+
+      // Iterar pelas propriedades do objeto JSON
+      for (let key in item) {
+        if (item.hasOwnProperty(key)) {
+          const span = document.createElement('span');
+          span.textContent = `${key}: ${item[key]}, `;
+          p.appendChild(span);
+        }
+      }
+
+      divDados.appendChild(p);
+    });
+  });
